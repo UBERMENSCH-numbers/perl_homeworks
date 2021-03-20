375a0452065e5c2d0cde445278c8561027815334
v.kotelnik <kotelnik.slava@gmail.com>
2021-03-20 11:59:45 +0300

    added h key

--- sort/sort.pl
+++ sort/sort.pl
@@ -2,6 +2,16 @@
 use 5.016;
 use feature 'fc';
 
+
+sub unsuffix($) {
+	my $str= lc shift;
+	if (index($str, "k") != -1) { substr($str, index($str, "k")) = ""; $str *= 1000 };
+	if (index($str, "m") != -1) { substr($str, index($str, "m")) = ""; $str *= 1000000 };
+	if (index($str, "g") != -1) { substr($str, index($str, "g")) = ""; $str *= 1000000000 };
+	if (index($str, "t") != -1) { substr($str, index($str, "t")) = ""; $str *= 1000000000000 };
+	return $str;
+}
+
 my @filenames = grep { !/-/ } @ARGV;
 my @keys = map { split (//, substr $_, 1, length $_) } grep { /-/ } @ARGV;
 my %keys_hash = map { $_ => 1 } @keys;
@@ -20,6 +30,11 @@ chomp $_ for (@input);
 my @data = map {[$_, $_]} @input;
 
 if ($keys_hash{'b'}) { $_->[0] =~ s/^\s+// for (@data) };
+
+if ($keys_hash{'h'}) {
+	@data = map { [ unsuffix($_->[0]) , $_->[1] ] } @data;
+	$keys_hash{'n'} = 1;
+}
 
 my %uniq;
 if ($keys_hash{'u'}) { @data = grep { !$uniq{$_->[0]}++ } @data };
