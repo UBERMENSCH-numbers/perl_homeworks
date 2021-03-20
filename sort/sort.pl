5a925fb12187fc830acb96537664e0eec65d6c60
v.kotelnik <kotelnik.slava@gmail.com>
2021-03-20 10:54:53 +0300

    small fix

--- sort/sort.pl
+++ sort/sort.pl
@@ -28,8 +28,9 @@ if ($keys_hash{'n'}) { @data = sort { $a->[0] <=> $b->[0]} @data };
 
 if ($keys_hash{'M'}) {
 	my %mon = (jan => 1, feb => 2, mar => 3, apr => 4, may => 5, jun => 6, jul => 7, aug => 8, sep => 9, oct => 10, nov => 11, dec => 12);
-	my %months = (january => 1, february => 2, march => 3, april => 4, may => 5, june => 6, july => 7, august => 8, september => 9, october => 10, november => 11, december => 12);
-	@data = sort { ($mon{lc($a->[0])} or $months{lc($a->[0])}) <=> ($mon{lc($b->[0])} or $months{lc($b->[0])}) } @data;
+	@data = sort { $mon{ lc substr($a->[0], 0, 3) } <=> $mon{ lc substr($b->[0], 0, 3) }
+		||
+	$b->[0] cmp $a->[0] } @data;
 }
 
 if ($keys_hash{'r'}) { @data = reverse @data };
