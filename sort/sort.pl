f24527a2663416b315da0b1bb53fb6a3daec9a1a
v.kotelnik <kotelnik.slava@gmail.com>
2021-03-20 17:10:26 +0300

    small fix

--- sort/sort.pl
+++ sort/sort.pl
@@ -2,13 +2,12 @@
 use 5.016;
 use feature 'fc';
 
-
 sub unsuffix($) {
-	my $str= lc shift;
-	if (index($str, "k") != -1) { substr($str, index($str, "k")) = ""; $str *= 1000 };
-	if (index($str, "m") != -1) { substr($str, index($str, "m")) = ""; $str *= 1000000 };
-	if (index($str, "g") != -1) { substr($str, index($str, "g")) = ""; $str *= 1000000000 };
-	if (index($str, "t") != -1) { substr($str, index($str, "t")) = ""; $str *= 1000000000000 };
+	my $str = lc shift;
+	if ( index($str, "k") != -1 ) { substr($str, index($str, "k")) = ""; $str *= 1000 };
+	if ( index($str, "m") != -1 ) { substr($str, index($str, "m")) = ""; $str *= 1000000 };
+	if ( index($str, "g") != -1 ) { substr($str, index($str, "g")) = ""; $str *= 1000000000 };
+	if ( index($str, "t") != -1 ) { substr($str, index($str, "t")) = ""; $str *= 1000000000000 };
 	return $str;
 }
 
@@ -17,8 +16,7 @@ my @keys = map { split (//, substr $_, 1, length $_) } grep { /-/ } @ARGV;
 my %keys_hash = map { $_ => 1 } @keys;
 my $col_ind = ( grep {/\d/} @keys )[0];
 
-
-if(%keys_hash{'M'} and %keys_hash{'n'}) { die "options '-Mn' are incompatible" };
+if ( %keys_hash{'M'} and %keys_hash{'n'} ) { die "options '-Mn' are incompatible" };
 
 my @input;
 if ($filenames[0]) {
@@ -33,7 +31,7 @@ my @data = map {[$_, $_]} @input;
 
 if ($keys_hash{'k'}) {
 	for (@data) {
-		$_->[0] = (split /[ \t]/, $_->[0])[$col_ind-1] ;
+		$_->[0] = ( split /[ \t]/, $_->[0] )[$col_ind-1] ;
 	}
 }
 
@@ -47,9 +45,9 @@ if ($keys_hash{'h'}) {
 my %uniq;
 if ($keys_hash{'u'}) { @data = grep { !$uniq{$_->[0]}++ } @data };
 
-my @data = sort {$a->[0] cmp $b->[0] } @data;
+my @data = sort { $a->[0] cmp $b->[0] } @data;
 
-if ($keys_hash{'n'}) { @data = sort { $a->[0] <=> $b->[0]} @data };
+if ($keys_hash{'n'}) { @data = sort { $a->[0] <=> $b->[0] } @data };
 
 if ($keys_hash{'M'}) {
 	my %mon = (jan => 1, feb => 2, mar => 3, apr => 4, may => 5, jun => 6, jul => 7, aug => 8, sep => 9, oct => 10, nov => 11, dec => 12);
