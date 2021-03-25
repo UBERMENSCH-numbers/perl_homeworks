301c947d6f866163004bcf24c3ff7a7de3f24a42
v.kotelnik <kotelnik.slava@gmail.com>
2021-03-25 20:56:41 +0300

    experiment

--- sort/sort.pl
+++ sort/sort.pl
@@ -61,7 +61,7 @@ if ($keys_hash{'n'}) {
 	my @nums = grep { $_->[0] =~ /^\-?\d+$/ } @data;
 	my %excl_hash = map { $_->[0] => 1 } @nums;
 	my @chars = grep { !$excl_hash{ $_->[0] } } @data;
-	my @chars_sort = sort { $a->[0] cmp $b->[0] } @not_nums;
+	my @chars_sort = sort { $a->[0] cmp $b->[0] } @chars;
 	@chars_sort = map { [0, $_->[1]] } @chars_sort;
 	@data = sort { $a->[0] <=> $b->[0] } (@nums, @chars_sort);
 }
