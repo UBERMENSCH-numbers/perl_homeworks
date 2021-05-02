f5315bcadc3eab49ed769772b45505defc1b777c
v.kotelnik <kotelnik.slava@gmail.com>
2021-05-02 12:02:22 +0300

    save

--- shell/shell.pl
+++ shell/shell.pl
@@ -3,45 +3,104 @@ use warnings;
 use Cwd qw(cwd);
 use Data::Dumper;
 use feature 'say';
+use Term::ANSIColor;
+use FileHandle;
 
-my @commands = split '\|',  $ARGV[0];
-my @results;
+$|=1;
+my %UTILS;
+my @stopped = ();
+my $foreground;
 
-my $prev;
-for my $i (0..$#commands) {
-    my @items = split " ", $commands[$i];
-    print Dumper(\@items);
-    if ($items[$i] eq "cd") {
-        cd(@items);
-        next;
-    } elsif ($items[$i] eq "pwd") {
-        $results[$i] = pwd();
-        next;
-    } elsif ($items[$i] eq "echo") {
-        $results[$i] = echo(@items);
-        next;
-    } elsif ($items[$i] eq "kill") {
-        kill_(@items);
-        next
-    } else {
-        if (defined $prev) {
-            $results[$i] = `$prev | $commands[$i]`;
-        } else {
-            $results[$i] = `$commands[$i]`;
-        }
+# $SIG{TSTP} = sub {
+#     say "catch";
+#     kill 23, $foreground or die "$foreground not found";
+#     push @stopped, $foreground;
+#     $foreground = 0;
+# };
+
+# $SIG{CONT} = sub {
+#     $foreground = pop @stopped;
+#     kill 25, $foreground or die "$foreground not found";
+# };
+
+
+%UTILS = (
+    "cd" => \&cd,
+    "pwd" => \&pwd,
+    "echo" => \&echo,
+    "kill" => \&kill_,
+    # "jobs" => \&jobs,
+);
+
+while (1) {
+    print color('bold green') . "[pShell " . color('bold white') . pwd() . color('bold green'). "]\$ ". color('reset');
+    my $input = <STDIN>;
+    chomp $input;
+
+    next unless (defined $input && $input ne "");
+
+    my $output = process($input);
+    if (defined $output) {
+        chomp $output;
+        say $output;
     }
-    $prev = $results[$i];
 }
 
-print Dumper(\@results);
 
+sub process {
+    my @commands = split /\Q|\E/, $_[0];
+    my ($output, $input) = (1, 1);
+
+    for my $i (0..$#commands) {
+        my @items = split " ", $commands[$i];
+        if (defined $UTILS{$items[0]}) {
+            return $UTILS{$items[0]}(@items);
+        } else {
+            if ($i == 0 && $#commands != 0) {
+                $input = fork_exec($commands[$i], undef, $output);
+            } elsif ($i != 0 && $i == $#commands) {
+                return fork_exec($commands[$i], $input, undef);
+            } elsif ($i != 0 && $i != $#commands) {
+                $input = fork_exec($commands[$i], $input, $output);
+            } else {
+                return fork_exec($commands[$i], undef, undef);
+            }
+        }  
+    }      
+}
+
+sub fork_exec {
+    my ($command, $input, $output) = @_;
+    
+    my $pid;
+    if (defined $output) {
+        die "could not fork\n" unless defined($pid = open(CHILD, "-|"));
+    } else {
+        die "could not fork\n" unless defined($pid = fork());
+    }
+
+    if ($pid) {
+        $foreground = $pid;
+        waitpid $pid, 0;
+        if (defined $output) {
+            my @out = <CHILD>;
+            close(CHILD);
+            return join "", @out
+        }
+        return undef;
+    } elsif ($pid == 0) {
+        exec("echo '$input' | $command") if (defined $input);
+        exec($command); 
+    }
+}
 
 sub cd {
     unless (-d $_[1]) {
-        die "dir $_[1] not exists";
+        warn "dir $_[1] not exists\n";
     } else {
         chdir("$_[1]") ;
     }
+    return undef;
 }
 
 sub pwd {
@@ -56,15 +115,10 @@ sub echo {
 sub kill_{
     shift;
     my $sig = shift =~ tr/-//;
-    kill $sig, shift;
+    warn "process not found\n" unless (kill $sig, shift);
+    return undef;
 }
 
-my $child_pid;
-if (!defined($child_pid = fork())) {
-  die "cannot fork: $!";
-
-} elsif ($child_pid) {
-    say "---parent---";
-} else {
-    say "---child---";
+sub jobs {
+    say for (@stopped);
 }
