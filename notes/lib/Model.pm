4133c9c246be13597d3af251881249c2471dacfc
v.kotelnik <kotelnik.slava@gmail.com>
2021-05-29 15:42:56 +0300

    commit

--- notes/lib/Model.pm
+++ notes/lib/Model.pm
@@ -3,18 +3,17 @@ package Model;
 use DBI;
 use strict;
 use warnings;
-use feature 'say';
 use Digest::MD5 'md5_hex';
  
-my $dbh = DBI->connect("dbi:SQLite:dbname=lib/notes.db","",""); 
+my $dbh = DBI->connect("dbi:SQLite:dbname=../notes/notes.db","","",{ sqlite_unicode => 1 });
 
 sub register_user {
     shift;
     my ($login, $password) = @_;
-    if ($dbh->selectrow_array("SELECT count(*) FROM users WHERE login = '$login'")) {
+    if ($dbh->selectrow_array("SELECT count(*) FROM users WHERE login = ?", undef, $login)) {
         return 0;
     } else {
-        $dbh->do("INSERT INTO users VALUES ('$login', '$password')");
+        $dbh->do("INSERT INTO users VALUES (?, ?)", undef, $login, $password);
         return 1;
     }
 }
@@ -22,7 +21,7 @@ sub register_user {
 sub login_user {
     shift;
     my ($login, $password) = @_;
-    if ($dbh->selectrow_array("SELECT count(*) FROM users WHERE login = '$login' AND password = '$password'")) {
+    if ($dbh->selectrow_array("SELECT count(*) FROM users WHERE login = ? AND password = ?", undef, $login, $password)) {
         return 1;
     }
     return 0;
@@ -32,7 +31,7 @@ sub new_note {
     shift;
     my ($title, $text, $user) = @_;
     my $id = md5_hex(rand());
-    $dbh->do("INSERT INTO notes(id, title, text, user) values('$id', '$title', '$text', '$user')");
+    $dbh->do("INSERT INTO notes(id, title, text, user) values(?, ?, ?, ?)", undef, $id, $title, $text, $user);
     return $id;
 
 }
@@ -40,18 +39,19 @@ sub new_note {
 sub add_share {
     shift;
     my ($id, $user) = @_;
-    $dbh->do("INSERT INTO share VALUES('$id','$user')");
+    $dbh->do("INSERT INTO share VALUES(?, ?)", undef, $id, $user);
 }
 
 sub edit_note {
     shift;
     my ($id, $title, $text) = @_;
+    use Data::Dumper;
     $dbh->do("
         UPDATE notes
-        set title = '$title',
-            text = '$text'
-        WHERE id = $id"
-    );
+        set title = ?,
+            text = ?
+        WHERE id = ?",
+    undef, $title, $text, $id);
 }
 
 sub edit_share {
@@ -59,26 +59,26 @@ sub edit_share {
     my $id = shift;
     my @users = @{+shift};
     
-    $dbh->do("DELETE FROM share WHERE id = '$id'");
-    $dbh->do("INSERT INTO share VALUES('$id','$_')") for @users;
+    $dbh->do("DELETE FROM share WHERE id = ?", undef, $id);
+    $dbh->do("INSERT INTO share VALUES(?, ?)", undef, $id, $_) for @users;
 }
 
 sub get_user_notes {
     shift;
     my $user = shift;
-    return $dbh->selectall_arrayref("SELECT id, title FROM notes WHERE user = '$user'");
+    return $dbh->selectall_arrayref("SELECT id, title FROM notes WHERE user = ?", undef, $user);
 }
 
 sub get_note {
     shift;
     my $id = shift;
-    return $dbh->selectrow_hashref("SELECT id, title, text, user FROM notes WHERE id = '$id'");
+    return $dbh->selectrow_hashref("SELECT id, title, text, user FROM notes WHERE id = ?", undef, $id);
 }
 
 sub get_share {
     shift;
     my $id = shift;
-    return $dbh->selectcol_arrayref("SELECT user FROM share WHERE id = '$id'");
+    return $dbh->selectcol_arrayref("SELECT user FROM share WHERE id = ?", undef, $id);
 }
 
 sub get_shared_by_user {
@@ -87,8 +87,14 @@ sub get_shared_by_user {
     return $dbh->selectall_arrayref("
         SELECT title, id, user
         FROM notes
-        WHERE id in (SELECT id from share WHERE user = '$user')"
-    );
+        WHERE id in (SELECT id from share WHERE user = ?)",
+    undef, $user);
+}
+
+sub delete_note {
+    shift;
+    my $id = shift;
+    $dbh->do("DELETE FROM notes WHERE id = ?", undef, $id);
 }
 
 sub new {
@@ -96,8 +102,3 @@ sub new {
     my %params = @_;
     return bless \%params, $self;
 }
-
-# use Data::Dumper;
-# say Dumper(Model->get_note(17));
-# say Dumper(Model->get_share(17));
-# Model->edit_share(17, ["user1","user2","user3"]);
--- notes/lib/notes.pm
+++ notes/lib/notes.pm
@@ -1,57 +1,35 @@
 package notes;
 use Dancer2;
-use Dancer2::Plugin::Database;
 use strict;
 use warnings;
-use feature 'say';
+use utf8;
 use Model;
 use Template;
-
-set 'session' => 'Simple';
-# set 'template' => 'template_toolkit';
 
 our $VERSION = '0.1';
 my $model = Model->new();
 
-any ['get', 'post'] => '/new_note' => sub {
-    if ( not session('logged_in') ) {
-        redirect '/login';
-    } elsif (request->method() eq "POST") {
-        my $username = session('user');
-        my $text = body_parameters->get('note_text');
-        my $title = body_parameters->get('note_title');
-        my @users = split ",", body_parameters->get('note_share');
-        my $inserted_id = $model->new_note($title, $text, $username);
-        for (@users) {
-            $_ =~ s/\s+//g;
-            $model->add_share($inserted_id, $_);
-        }
-        redirect '/';
-    }
-    template 'note', {note_title => "Title", note_text => "text", note_share => "user1,user2"};
-};
-
 any ['get', 'post'] => '/login' => sub {
-    if ( request->method() eq "POST" ) {
-        my $username = body_parameters->get('username');   
+    if (request->method() eq "POST") {
+        my $username = body_parameters->get('username');
         my $password = body_parameters->get('password');
         if ($model->login_user($username, $password)) {
             session 'logged_in' => true;
             session 'user' => $username;
-            return redirect '/';
+            redirect param('path') || '/';
         }
     }
     template 'login';
 };
 
 any ['get', 'post'] => '/register' => sub {
-    if ( request->method() eq "POST" ) {
-        my $username = body_parameters->get('username');   
+    if (request->method() eq "POST") {
+        my $username = body_parameters->get('username');
         my $password = body_parameters->get('password');
         if ($model->register_user($username, $password)) {
             session 'logged_in' => true;
             session 'user' => $username;
-            return redirect '/';
+            redirect param('path') || '/';
         }
     }
     template 'register';
@@ -64,13 +42,17 @@ get '/logout' => sub {
 
 hook before => sub {
     if (!session('user') && request->path_info !~ m{^/login} && request->path_info !~ m{^/register}) {
-        redirect '/login';
+        redirect '/login?path='.request->path_info;
     }
+};
+
+post '/delete_note' => sub {
+    $model->delete_note(query_parameters->get('id'));
+    redirect '/';
 };
 
 get '/' => sub {
     my $username = session('user');
-    redirect '/login' unless defined $username;
     my $notes = $model->get_user_notes($username);
     my $share = $model->get_shared_by_user($username);
     my %notes;
@@ -81,17 +63,20 @@ get '/' => sub {
     my %shared;
     for (@{$share}) {
         my ($title, $id, $user) = @{$_};
-        $shared{$id} = [$title, $user]; 
+        $shared{$id} = [$title, $user];
     }
-
-    template 'main', {notes => \%notes, notes_shared => \%shared};
+    template 'main', {
+        notes => \%notes,
+        notes_shared => \%shared,
+        username => $username
+    };
 };
 
 any ['get', 'post'] => '/note' => sub {
     my $id = query_parameters->get('id');
     my $username = session('user');
 
-    if ( request->method() eq "POST" ) {
+    if (request->method() eq "POST") {
         my $text = body_parameters->get('note_text');
         my $title = body_parameters->get('note_title');
         my @users = split ",", body_parameters->get('note_share');
@@ -101,11 +86,11 @@ any ['get', 'post'] => '/note' => sub {
         redirect '/';
     }
 
-
     my $note = $model->get_note($id);
     redirect '/' unless $note;
-    my $share = join ",", @{$model->get_share($id)};
     my $readonly = $username ne $note->{user};
+    my $share = join ",", @{$model->get_share($id)};
+
     template 'note', {
         note_title => $note->{title},
         note_text => $note->{text},
@@ -114,6 +99,24 @@ any ['get', 'post'] => '/note' => sub {
     };
 };
 
+any ['get', 'post'] => '/new_note' => sub {
+    if (request->method() eq "POST") {
+        my $username = session('user');
+        my $text = body_parameters->get('note_text');
+        my $title = body_parameters->get('note_title');
+        my @users = split ",", body_parameters->get('note_share');
+        my $inserted_id = $model->new_note($title, $text, $username);
+        for (@users) {
+            $_ =~ s/\s+//g;
+            $model->add_share($inserted_id, $_);
+        }
+        redirect '/';
+    }
+    template 'note', { 
+        note_title => "Title",
+        note_text => "text",
+        note_share => "user1,user2"
+    };
+};
 
 1;
-## TODO сохранение пути до редиректа на логин
