3f6e9d215d3e525887e53d304f917a318e19a7ee
v.kotelnik <kotelnik.slava@gmail.com>
2021-06-12 17:31:33 +0300

    done

--- bus/bus.pl
+++ bus/bus.pl
@@ -17,8 +17,9 @@ use Data::Dumper;
 use feature 'say';
 use utf8;
 
-my $address = '100.100.145.77';
+my $address = '100.100.147.21';
 my $listen_port = 8888;
+my $N_MSGS_POOL_SIZE = 50;
 
 my $cb_connect;
 my $cb_feeder_msg;
@@ -32,76 +33,95 @@ my @msgs = ();
 
 sub send_new {
     my $msg = shift;
-    # print "-----------\n";
-    # print Dumper($msg);
-    # print Dumper($readers);
-    # print Dumper($feeders);
-    # print "-----------\n";
     for (keys %$readers) {
         if ($msg->{type} eq $readers->{$_}->{type}) {
             $readers->{$_}->{client}->push_write("New messsage:\n");
-            $readers->{$_}->{client}->push_write(json => ($msg));
+            $readers->{$_}->{client}->push_write(json => $msg);
         }
     }
 };
 
+sub on_error {
+    my ($client, $fatal, $msg) = @_;
+    my $client_str = "$client";
+    my $key = "$client->{fh}";
+    warn "error from socket $client : $msg";
+    unless ($fatal) { 
+        $clients->{$key}->push_write($msg);
+    }
+    close($client->{fh});
+    $clients->{$key}->destroy;
+    warn "destroyed $client";
+    delete $clients->{$client_str};
+    delete $feeders->{$client_str};
+    delete $readers->{$client_str};
+    print(Dumper($readers), Dumper($feeders));
+}
+
 $cb_feeder_msg = sub {
     my ($client, $buf) = @_;
     if (!$buf->{type} || !$buf->{msg}) {
-        $client->push_write("invalid format");
+        on_error($client, 1, "Invalid JSON?");
         return;
     }
     $buf->{feeder} = $feeders->{$client}{name};
+    shift @msgs if ($#msgs >= $N_MSGS_POOL_SIZE - 1);
     push @msgs, $buf;
     $client->push_write("Your message:\n");
-    $client->push_write(json => ($buf));
+    $client->push_write(json => $buf);
     send_new($buf);
     $client->push_read(json => $cb_feeder_msg);
     # print Dumper(\@msgs);
-    
 };
+
+my $HASH_REF_STR = ref {};
 
 $cb_connect = sub {
 	my ($client, $buf) = @_;
 	warn "Read from client\n". Dumper($buf);
-    unless (ref $buf eq ref {} && $buf->{stream} && ($buf->{name} || $buf->{type})) {
-        $client->push_write("Missed 'stream' or 'type'\n You leaved the Dark Side\n");
+    unless (ref $buf eq $HASH_REF_STR && $buf->{stream} && ($buf->{name} || $buf->{type})) {
+        $client->push_write("Missed 'name' or 'type'/'name' \n You leaved the Dark Side\n");
         $client->push_shutdown();
         return;
     }
     if ($buf->{stream} eq "in") {
-        say "new feeder". Dumper($client) . "\n";
-        %{$feeders->{$client}} = (client => $client, name => $buf->{name});
+        say "new feeder". Dumper("$client->{fh}");
+        $feeders->{$client} = {client => $client, name => $buf->{name}};
         $feeders->{$client}{client}->push_read(json => $cb_feeder_msg);
     } elsif($buf->{stream} eq "out") {
-        say "new reader". Dumper($client) . "\n";
-        %{$readers->{$client}} = (client => $client, type => $buf->{type});
+        say "new reader". Dumper("$client->{fh}");
+        $readers->{$client} = {client => $client, type => $buf->{type}};
         my @filtered = grep {$_->{type} eq $buf->{type}} @msgs;
         $readers->{$client}{client}->push_write("Your Type Messages:\n");
-        $readers->{$client}{client}->push_write(json => \@filtered);
+        $readers->{$client}{client}->push_write(json => $_) for @filtered;
+        my $new_line_cnt = 1;
+        my $reader_wd; $reader_wd = sub { 
+            $readers->{$client}{client}->push_read(chunk => 1, sub {
+                my ($fh, $data) = @_; 
+                say "`" . $data . "`\n";
+                if (!$data || !$new_line_cnt) {
+                    on_error(@_);
+                } else {
+                    $reader_wd->();
+                }
+                $new_line_cnt-- if $data eq "\n";
+            });
+        };
+        $reader_wd->();
+        
     }
 };
 
 my $cb_new_connection = sub {
-	my ($client, $host, $port) = @_;
-	my $hdl; $hdl = AnyEvent::Handle->new(
-		fh => $client,
-		on_error => sub {
-            my (undef, $fatal, $msg) = @_;
-            warn "error from socket: $msg";
-            unless ($fatal) { 
-                $hdl->push_write("Invalid JSON?\nYou leaved the Dark Side\n");
-                $hdl->destroy;
-            }
-            warn $hdl;
-            $hdl = undef;
-            close($client);
-        },
+	my ($fh, $host, $port) = @_;
+	$clients->{$fh} = AnyEvent::Handle->new(
+		fh => $fh,
+		on_error => \&on_error,
+        on_eof => \&on_error,
 	);
-	warn "New connection in $host:$port from client $client";
-    $hdl->push_write($ascii);
-    $clients->{$client} = $hdl;
-	$hdl->push_read(json => $cb_connect);
+	warn "New connection in $host:$port from client $fh";
+    $clients->{$fh}->push_write($ascii);
+	$clients->{$fh}->push_read(json => $cb_connect);
     return;
 };
 
